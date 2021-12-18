e3be3062efd15dff21a6d9808d8b9a9edd15e6d5
Require <68991107+1e17@users.noreply.github.com>
2021-12-17 20:04:28 -0800

    Update init.lua

--- hotkey/init.lua
+++ hotkey/init.lua
@@ -6,12 +6,13 @@ local hotkey = {
 
 local connection = userInputService.InputBegan:Connect(function(input, gameProcessedEvent)
   if not gameProcessedEvent and input.UserInputType == Enum.UserInputType.Keyboard then
-    hotkey.hotkeys[input.KeyCode]()
+    local key = tostring(input.KeyCode):upper():split('Enum.KeyCode.')[2]
+    hotkey.hotkeys[key]()
   end
 end)
 
 function hotkey.add(key, callback)
-  hotkey.hotkeys[key] = callback 
+  hotkey.hotkeys[key:upper()] = callback 
   return ({}).close()
     hotkey.hotkeys[key] = nil 
   end 
