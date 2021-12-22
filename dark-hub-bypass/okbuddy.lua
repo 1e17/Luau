728c2a67bda17f52f73c9ff9cf919caaf468defd
Require <68991107+1e17@users.noreply.github.com>
2021-12-21 21:17:33 -0800

    Create okbuddy.lua

---
+++ dark-hub-bypass/okbuddy.lua
@@ -0,0 +1,24 @@
+setreadonly(syn,false)
+local req = syn.request
+syn.request = function(args)
+  if args.Url == 'https://darkhub.maxt.church/checkKey' then 
+    return {
+      Body = 'OK'
+    }
+  end 
+  return req(args)
+end 
+local isFile = isfile
+isFile = hookfunc(isfile,function(str)
+  if str:find('DarkHubKey.DARKHUB') then 
+    return true 
+  end 
+  return isFile(str)
+end)
+local readFile = readfile 
+readFile = hookfunc(readfile,function(str)
+  if str:find('DarkHubKey.DARKHUB') then 
+    return 'cheese'
+  end 
+  return readFile(str)
+end)
