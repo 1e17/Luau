b7579a77df2d9f740645b203cdc83e914ae14540
Require <68991107+1e17@users.noreply.github.com>
2022-06-15 15:29:45 -0700

    Update darkHubLoader.lua

--- dark-hub-bypass/darkHubLoader.lua
+++ dark-hub-bypass/darkHubLoader.lua
@@ -1,7 +1,9 @@
+getgenv().DarkHubLoaded = true
 local http = game:GetService('HttpService')
 
+local mainDirectory = 'https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/'
 local directory = 'https://raw.githubusercontent.com/1e17/Luau-Misc/main/DarkHubGames/'
-local games_ = http:JSONDecode(game:HttpGet(directory .. 'SupportedGames'))
+local games_ = http:JSONDecode(game:HttpGet(mainDirectory .. 'SupportedGames'))
 
 local games = {}
 for c,k in next, games_ do 
@@ -11,4 +13,4 @@ end
 local game_ = games[game.PlaceId]
 if not game_ then game:GetService('Players').LocalPlayer:Kick('Game not supported') end
 
-loadstring(game:HttpGet(directory .. game_))()
+loadstring(game:HttpGet(directory .. game_ .. '.lua'))()
